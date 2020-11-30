5e725616df791fc7a6a0fc7cc45b916e00b247a3
Mateusz Grzeliński <grzelinskimat@gmail.com>
2020-11-30 19:42:25 +0100

    Add default row for predicting commands not existing in the training dataset by pswedrak

--- addons/blender_mind/models/iola.py
+++ addons/blender_mind/models/iola.py
@@ -11,6 +11,7 @@ class IOLA(PredictionModel):
         self.command_to_index = {}
         self.index_to_command = {}
         self.c = []
+        self.default_row = [1]
         self.alpha = 0.8
         self.initial_value = 1
         self.previous_command = None
@@ -25,6 +26,8 @@ class IOLA(PredictionModel):
                 for i in range(len(self.command_to_index) - 1):
                     self.c[i].append(min(self.c[i]))
 
+                self.default_row.append(min(self.default_row))
+
         if self.previous_command is None:
             self.previous_command = current_command
             return
@@ -34,9 +37,12 @@ class IOLA(PredictionModel):
 
         for i in range(len(self.c[previous_command_index])):
             self.c[previous_command_index][i] = self.c[previous_command_index][i] * self.alpha
+            self.default_row[i] = self.default_row[i] * self.alpha
 
         self.c[previous_command_index][current_command_index] \
             = self.c[previous_command_index][current_command_index] + (1 - self.alpha)
+
+        self.default_row[current_command_index] = self.default_row[current_command_index] + (1 - self.alpha)
 
         self.previous_command = current_command
 
@@ -44,7 +50,7 @@ class IOLA(PredictionModel):
         try:
             row = self.c[self.command_to_index[current_command]]
         except KeyError:
-            return []
+            row = self.default_row
         indices = [i for i in range(0, len(row))]
         values_dict = dict(zip(indices, row))
         sorted_dict = {k: v for k, v in sorted(values_dict.items(), key=lambda item: item[1], reverse=True)}
