26aaf93a9e8d60815c25dff59ab849ca272378be
pswedrak <piotrekswedrak@gmail.com>
2021-01-01 22:23:46 +0100

    Code refactor

--- iola.py
+++ iola.py
@@ -1,67 +1,72 @@
 import matplotlib.pyplot as plt
 import collections
+import numpy as np
+import seaborn as sns
 
 
 def main():
     commands = load_history("unixhist.txt")
+    top = [1, 2, 3, 4, 5, 6, 7, 8]
+    # model = Model(0.8)
+    # train_commands = commands[:round(len(commands) / 2)]
+    # eval_commands = commands[round(len(commands) / 2):]
+    # for command in train_commands:
+    #     model.update(command)
+    # show_model(model, commands, eval_commands)
+    # plot_heatmap(model)
+    # evaluate_most_common_commands(commands)
+    # find_most_common_pairs(commands)
+    # find_most_ofter_used_commands(model, commands, eval_commands, top)
+    # find_least_often_used_commands(model, commands, eval_commands, top)
+
+    alphas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    make_evaluations_for_alpha(alphas, top, commands)
+
+
+def make_evaluations_for_alpha(alphas, top, commands):
     train_commands = commands[:round(len(commands) / 2)]
     eval_commands = commands[round(len(commands) / 2):]
-    model = Model()
 
-    for command in train_commands:
-        model.update(command)
+    lines = []
+    for alpha in alphas:
+        model = Model(alpha)
+        for command in train_commands:
+            model.update(command)
 
-    print(f'All commands (dataset) {len(commands)}')
-    different_commands = len(set(commands))
-    print(f'Different commands {different_commands}')
-    print(f'Final evaluation (prediction is in top 3): {evaluate_model(model, eval_commands, top_n=3)}')
+        evaluations = [evaluate_model(model, eval_commands, top_n=t) for t in top]
+        line, = plt.plot(top, evaluations, label="alpha = " + str(alpha))
+        lines.append(line)
+        print("Model has been evaluated for alpha: " + str(alpha))
 
-    # most common commands:
-    counter = collections.Counter(commands)
-    print(f'Top 5 commands: {counter.most_common(5)}')
-    counter_items = sorted(counter.items(), key=lambda i: i[1])
-    plt.pie(
-        [v for k, v in counter_items],
-        labels=[k for k, v in counter_items]
-    )
-    plt.title('Commands in data')
-    plt.show()
-
-    # most common pairs
-    commands_in_pairs = []
-    for command, next_command in zip(commands, commands[1:]):
-        commands_in_pairs.append(f'{command}; {next_command}')
-    counter = collections.Counter(commands_in_pairs)
-    print(f'Top 5 pairs: {counter.most_common(5)}')
-    counter_items = sorted(counter.items(), key=lambda i: i[1])
-    plt.pie(
-        [v for k, v in counter_items],
-        labels=[k for k, v in counter_items]
-    )
-    plt.title('Most common command pairs')
-    plt.show()
-
-    top = [1, 2, 3, 4, 5, 6, 7, 8]
-    evaluations = [evaluate_model(model, eval_commands, top_n=t) for t in top]
-    plt.plot(top, evaluations)
     plt.xlabel('Top n predictions')
     plt.ylabel('Success rate')
     plt.title('Prediction success rate (all commands)')
+    plt.legend(handles=lines)
     plt.show()
 
-    # most often used commands
+
+def find_least_often_used_commands(model, commands, eval_commands, top):
     counter = collections.Counter(commands)
+    different_commands = len(set(commands))
+    least_frequent_commands = set(k for k, v in counter.most_common()[:round(different_commands * 0.5):-1])
+    print(f'Least 10% of all different commands ({len(least_frequent_commands)}/{different_commands}): {least_frequent_commands}')
+    evaluations = [
+        evaluate_model(model, eval_commands, top_n=t, predict_only_commands=least_frequent_commands)
+        for t in top
+    ]
+
+    plt.plot(top, evaluations)
+    plt.xlabel('Top n predictions')
+    plt.ylabel('Success rate')
+    plt.title(f'Prediction success rate ({len(least_frequent_commands)} least frequent commands)')
+    plt.show()
+
+
+def find_most_ofter_used_commands(model, commands, eval_commands, top):
+    counter = collections.Counter(commands)
+    different_commands = len(set(commands))
     most_frequent_commands = set(k for k, v in counter.most_common()[:round(different_commands * 0.1)])
-    print(
-        f'Top 10% of all different commands ({len(most_frequent_commands)}/{different_commands}): {most_frequent_commands}')
-    # take commands, that count for 50% of all data
-    # most_frequent_commands = set()
-    # usegaes = 0
-    # for command, nr_of_usages in counter.most_common():
-    #     if usegaes > len(commands) / 2:
-    #         break
-    #     usegaes += nr_of_usages
-    #     most_frequent_commands.add(command)
+    print(f'Top 10% of all different commands ({len(most_frequent_commands)}/{different_commands}): {most_frequent_commands}')
 
     evaluations = [
         evaluate_model(model, eval_commands, top_n=t, predict_only_commands=most_frequent_commands)
@@ -74,28 +79,44 @@ def main():
     plt.title(f'Prediction success rate ({len(most_frequent_commands)} most frequent commands)')
     plt.show()
 
-    # least often used commands
+
+def show_model(model, commands, eval_commands):
+    print(f'All commands (dataset) {len(commands)}')
+    different_commands = len(set(commands))
+    print(f'Different commands {different_commands}')
+    print(f'Final evaluation (prediction is in top 3): {evaluate_model(model, eval_commands, top_n=3)}')
+
+
+def plot_heatmap(model):
+    weights = np.array(model.c)
+    ax = sns.heatmap(weights)
+    plt.show()
+
+
+def evaluate_most_common_commands(commands):
     counter = collections.Counter(commands)
-    least_frequent_commands = set(k for k, v in counter.most_common()[:round(different_commands * 0.5):-1])
-    print(
-        f'Least 10% of all different commands ({len(least_frequent_commands)}/{different_commands}): {least_frequent_commands}')
-    # take commands, that count for 10% of all data
-    # usegaes = 0
-    # for command, nr_of_usages in counter.most_common()[::-1]:
-    #     if usegaes > len(commands) / 10:
-    #         break
-    #     usegaes += nr_of_usages
-    #     least_frequent_commands.add(command)
+    print(f'Top 5 commands: {counter.most_common(5)}')
+    counter_items = sorted(counter.items(), key=lambda i: i[1])
+    plt.pie(
+        [v for k, v in counter_items],
+        labels=[k for k, v in counter_items]
+    )
+    plt.title('Commands in data')
+    plt.show()
 
-    evaluations = [
-        evaluate_model(model, eval_commands, top_n=t, predict_only_commands=least_frequent_commands)
-        for t in top
-    ]
 
-    plt.plot(top, evaluations)
-    plt.xlabel('Top n predictions')
-    plt.ylabel('Success rate')
-    plt.title(f'Prediction success rate ({len(least_frequent_commands)} least frequent commands)')
+def find_most_common_pairs(commands):
+    commands_in_pairs = []
+    for command, next_command in zip(commands, commands[1:]):
+        commands_in_pairs.append(f'{command}; {next_command}')
+    counter = collections.Counter(commands_in_pairs)
+    print(f'Top 5 pairs: {counter.most_common(5)}')
+    counter_items = sorted(counter.items(), key=lambda i: i[1])
+    plt.pie(
+        [v for k, v in counter_items],
+        labels=[k for k, v in counter_items]
+    )
+    plt.title('Most common command pairs')
     plt.show()
 
 
@@ -118,12 +139,12 @@ def evaluate_model(model, commands, top_n=3, predict_only_commands: set = None):
 
 
 class Model:
-    def __init__(self):
+    def __init__(self, alpha):
         self.command_to_index = {}
         self.index_to_command = {}
         self.c = []
         self.default_row = [1]
-        self.alpha = 0.8
+        self.alpha = alpha
         self.initial_value = 1
         self.previous_command = None
 
